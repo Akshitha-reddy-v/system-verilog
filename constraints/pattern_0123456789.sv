1494b173b418f004a0c1b03ce286acf3e78ad484
Akshitha-reddy-v <142597246+Akshitha-reddy-v@users.noreply.github.com>
2025-06-01 00:33:40 +0530

    Add files via upload

---
+++ constraints/pattern_0123456789.sv
@@ -0,0 +1,22 @@
+
+// pattern - 0123456789
+
+module test;
+  class pattern;
+    rand int da[];
+    constraint da_size { da.size==10; }
+    constraint pattern_gen { foreach(da[i])
+      							da[i]==i;
+                           }
+  endclass : pattern
+  pattern p1;
+  initial begin
+    p1 = new;
+    assert(p1.randomize());
+    $display("%0p",p1.da);
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+endmodule
